0007ae6d020a073bc2b9171dc0055e66812c532f
Multfun <qtvfun@gmail.com>
2020-03-22 22:51:04 +0200

    sdsdsd

---
+++ sw-register.js
@@ -0,0 +1,17 @@
+//This is the "Offline page" service worker
+
+// Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+if (navigator.serviceWorker && navigator.serviceWorker.controller) {
+    console.log('[PWA Builder] active service worker found, no need to register');
+  }
+  else {
+    // Check for service worker support and
+    // register the ServiceWorker if we have it
+    if (navigator.serviceWorker) {
+      navigator.serviceWorker.register('sw.js', {
+        scope: './'
+      }).then((reg) => {
+        console.log('Service worker has been registered for scope: ' + reg.scope);
+      });
+    }
+  }
